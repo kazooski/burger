8e8338fb27e990fffc8f18b80fabb2d2b0b651a7
noisette <kasusky.music@gmail.com>
2020-07-17 00:39:03 -0700

    Created and exported connection. D sqls,

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burger_db;
 
 CREATE TABLE burgers
 (
-    id int NOT NULL AUTO INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
